fc40ed3431627cae1fea0e0b3232f573bdb65a14
AsadulIslamRipon <mdasadulripon@gmail.com>
2023-09-11 15:13:54 +0600

    structuring the code and find findOne FeildFIltering method

--- src/app.ts
+++ src/app.ts
@@ -1,8 +1,10 @@
 import cors from "cors";
-import express, { Application, NextFunction, Request, Response } from "express";
-import { Schema, model } from "mongoose";
+import express, { Application } from "express";
 
 const app: Application = express();
+
+//Application routes
+import userRoutes from "./app/modules/user/user.route";
 
 // using cors
 app.use(cors());
@@ -15,112 +17,16 @@ app.use(
   })
 );
 
-app.get("/", (req: Request, res: Response, next: NextFunction) => {
-  // inserting a test a data mongodb
-  /*
-    step1. Interface
-    step2. Schema,
-    step3. Model,
-    step4. Database Query
-      */
-
-  //   res.send("Hello World!");
-  //   next();
-  // Creating Interface
-
-  interface IUser {
-    id: string;
-    role: "student";
-    password: string;
-    name: {
-      firstName: string;
-      middleName?: string;
-      lastName: string;
-    };
-    dateOfBirth?: string;
-    gender: "male" | "female";
-    email?: string;
-    contactNo: string;
-    emergencyContactNo: string;
-    presentAddress: string;
-    permanentAddress: string;
-  }
-  //Creating Schema using interface
-  const userSchema = new Schema<IUser>({
-    id: {
-      type: String,
-      required: true,
-      unique: true,
-    },
-    role: {
-      type: String,
-      required: true,
-    },
-    password: {
-      type: String,
-      required: true,
-    },
-    name: {
-      firstName: {
-        type: String,
-        required: true,
-      },
-      middleName: {
-        type: String,
-      },
-      lastName: {
-        type: String,
-        required: true,
-      },
-    },
-    dateOfBirth: {
-      type: String
-    },
-    gender: {
-      type: String,
-      enum: ["male", "female"],
-    },
-    email: {
-      type: String,
-    },
-    contactNo: {
-      type: String,
-      required: true,
-    },
-    emergencyContactNo: {
-      type: String,
-      required: true,
-    },
-    presentAddress: {
-      type: String,
-      required: true,
-    },
-    permanentAddress: {
-      type: String,
-      required: true,
-    },
-  });
-
-  const User = model<IUser>("User", userSchema);
-  const createUserToDB = async () => {
-    const user = new User({
-      id: 101,
-      role: "Footballer",
-      password: "123456",
-      name: {
-        firstName: "Tanmoy",
-        lastName: "Das",
-      },
-      gender: "male",
-      contactNo: "0111111",
-      emergencyContactNo: "011",
-      presentAddress: "Brazil",
-      permanentAddress: "Africa",
-    });
-    await user.save();
-    console.log(user);
-  };
-  createUserToDB();
-});
+app.use("/api/v1/user", userRoutes);
 
 export default app;
+
+//Pattern MVC, Model
+
+/**
+ * Interface > interface.ts
+ * Schema, Model > model.ts
+ *
+ * route >
+ * route function > controller.ts
+ */
